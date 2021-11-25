c9926cb7d1481bb11b04994c2f175f129d0cd5cb
Didier <didier.dourneau@yahoo.com>
2021-11-25 11:13:36 +0100

    récupération du nom de la ville

--- assets/js/meteoAPI.js
+++ assets/js/meteoAPI.js
@@ -1,16 +1,32 @@
 //récupération des éléments DOM
-let image = document.querySelector("#image");
-let posX = document.querySelector("#posX");
-let posY = document.querySelector("#posY");
+let city = document.querySelector("#city");
+let form = document.querySelector("#form");
+let button = document.querySelector("#button");
 
 // creation de l'url
 const url1 = "http://api.openweathermap.org/data/2.5/forecast?";
-const url2 = document.querySelector(Récupérer la ville dans l'input)
+//const url2 = document.querySelector(Récupérer la ville dans l'input);
 const url3 = "&appid=06a1a61f5da022dda5482cabfeee6442";
+
+// Ecouter les boutons play/pause et pbInput
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event);
+    console.log(event.target.city.value);
+    const name = event.target.city.value;
+    console.log(name);
+    buildUrl(name);
+});
+ 
+
+
+
 
 /**
  * fonction qui demande une photo avec fetch
  */
+
+/** 
 function askImage() {
     fetch("https://api.thecatapi.com/v1/images/search")
     .then(function(response) {
@@ -33,7 +49,6 @@ function askImage() {
 //écoute le bouton pour detecter le clic
 helloBtn.addEventListener("click", askImage);
 
-
-
+*/
 
 //[{"breeds":[],"id":"AUsOY15KJ","url":"https://cdn2.thecatapi.com/images/AUsOY15KJ.jpg","width":5392,"height":3595}]
